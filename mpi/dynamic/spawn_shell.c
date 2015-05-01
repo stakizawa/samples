4ef75f444a4fd99e2ab37046ea776c653f38b3e5
Shinichiro Takizawa <s.takizawa@gmail.com>
2015-05-01 18:46:22 +0900

    cleanup

--- mpi/dynamic/spawn_shell.c
+++ mpi/dynamic/spawn_shell.c
@@ -9,7 +9,7 @@ static char *worker_wrapper = "./mpi_wrap.sh";
 int main(int argc, char **argv) {
     int world_size, *universe_size, flag;
     MPI_Comm everyone;
-    char *cmd_args[2] = { worker, NULL};
+    char *cmd_args[2] = { worker, NULL };
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
