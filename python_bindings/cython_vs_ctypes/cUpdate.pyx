f380a5094c510edc59c9ed2022fdeeb45325d6de
Shinichiro Takizawa <s.takizawa@gmail.com>
2015-07-28 17:57:59 +0900

    implement print_values python wrapper by Cython

--- python_bindings/cython_vs_ctypes/cUpdate.pyx
+++ python_bindings/cython_vs_ctypes/cUpdate.pyx
@@ -1,8 +1,16 @@
+from libc.stdlib cimport malloc, free
 cimport update
 
 class Updater(object):
     def __init__(self):
         super(Updater, self).__init__()
+
+    def print_values(self, ary):
+        cdef int *cary = <int*>malloc(sizeof(int) * len(ary))
+        for i in range(len(ary)):
+            cary[i] = ary[i]
+        update.print_values(cary, len(ary))
+        free(cary)
 
     def version(self):
         return update.version()
--- python_bindings/cython_vs_ctypes/update.pxd
+++ python_bindings/cython_vs_ctypes/update.pxd
@@ -1,2 +1,3 @@
 cdef extern from 'update.h':
+    void print_values(int *vals, size_t count)
     char *version()
